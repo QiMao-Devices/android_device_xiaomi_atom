3c8fc09789922f82e7f5c5473b0fff281d4b61fd
HuaLiMao-Studio <hualimao_aq@163.com>
2022-05-01 01:48:51 +0800

    atom: Initial skeleton tree
    Signed-off-by: HuaLiMao-AQ <hualimao_aq@163.com>
    Change-Id: I59fb4d04ef097071c2dd43e4c26c542e6b80916e

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2022 Project-QiMao
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/qimao_atom.mk
+
+COMMON_LUNCH_CHOICES := \
+	qimao_atom-user \
+	qimao_atom-userdebug \
+	qimao_atom-eng

a7583faffadda9563968b56285a19338713f149e
HuaLiMao-Studio <hualimao_aq@163.com>
2022-05-02 18:32:33 +0800

    atom: Import Mediatek permissions
    Signed-off-by: HuaLiMao-AQ <hualimao_aq@163.com>
    Change-Id: Ie0dca312fe856903fdcfc7a894f6cf6040c7e2fc

--- device.mk
+++ device.mk
@@ -42,3 +42,7 @@ PRODUCT_PACKAGES += \
 # XiaomiParts
 PRODUCT_PACKAGES += \
     XiaomiParts
+
+# Permissions
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/permissions/privapp-permissions-mediatek.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-mediatek.xml 

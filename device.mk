986291e4a41de2e2dc451db5ea97a0149b16c275
wbs306 <wbs306@gmail.com>
2022-08-19 23:39:48 +0800

    atom: Import powerscntbl.xml from stock
    * power: Rename MTKPOWER_HINT_UX_SCROLLING hint to INTERACTION
    * power: Add in MTK APP TOUCH hints to INTERACTION hint
    
    Co-authored-by: SamarV-121 <samarvispute121@pm.me>
    Co-authored-by: Vaisakh Murali <mvaisakh@statixos.com>

--- device.mk
+++ device.mk
@@ -69,6 +69,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/permissions/privapp-permissions-mediatek.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-mediatek.xml 
 
+# Power
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/power/powerscntbl.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/powerscntbl.xml
+
 # Telephony
 PRODUCT_BOOT_JARS += \
     mediatek-common \
